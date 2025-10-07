c461cfc9bb52b44c3a35b6d092052906b9cc56fb
홍기문(Kimoon Hong) <153352715+KimoonH@users.noreply.github.com>
2025-10-07 19:20:04 +0900

    Initial commit

---
+++ src/main/java/com/dsg/deploybackendpost/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.dsg.deploybackendpost.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://52.79.123.55:3000", "http://dsgpost.shop", "https://dsgpost.shop", "https://api.dsgpost.shop") // Next.js 기본 포트
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .maxAge(3600) // 1시간 동안 preflight 요청 결과를 캐시
+                .allowCredentials(true);
+    }
+}
+
